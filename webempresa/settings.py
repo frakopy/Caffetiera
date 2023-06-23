d5a9d0179ad50d59e3eb53aba6a1c7490f6e4304
frakopy <frako789@gmail.com>
2023-06-23 18:39:58 -0500

    Setting Debug to True

--- webempresa/settings.py
+++ webempresa/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-mne^key%3al-!4vgieo&g7ff-f37ep-yp25x5oh+l!@c98fqwc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
